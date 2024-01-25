e3a2867df2912e204532cf11d118faea082a864d
Ngoc Tran <newbiettn@users.noreply.github.com>
2024-01-25 19:31:03 +1100

    Swap neo-tree with nvim-tree

---
+++ plugins/nvim-tree.lua
@@ -0,0 +1,34 @@
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({
+        actions = {
+          open_file = {
+            resize_window = false,
+          },
+        },
+        lazy = false,
+      })
+    end,
+    keys = {
+      {
+        "<C-n>",
+        function()
+          vim.cmd([[NvimTreeToggle]])
+        end,
+        mode = { "n", "t" },
+        desc = "Toggle nvim-tree",
+      },
+      {
+        "<C-f>",
+        function()
+          vim.cmd([[NvimTreeFindFile]])
+        end,
+        desc = "Show current file in nvim-tree",
+      },
+    },
+  },
+}
